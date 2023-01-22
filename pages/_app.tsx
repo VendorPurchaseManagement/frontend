0cf9ebf76764a77ea2e7ae7f74fcdb0e1272b0dd
shashwat2003 <info.superpcstudio@gmail.com>
2023-01-22 11:10:50 +0530

    update baseURL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import {StyleProvider} from "@ant-design/cssinjs";
 import {NextPage} from "next";
 import type {AppProps} from "next/app";
-import {ReactElement, ReactNode} from "react";
+import {ReactElement, ReactNode, useEffect} from "react";
 import {Toaster} from "react-hot-toast";
 import {MainLayout} from "../common/layout";
+import {getRequest} from "../common/network";
+import {URLs} from "../common/network/URLs";
 import "../styles/globals.css";
 export type NextPageWithLayout = NextPage & {
   // eslint-disable-next-line no-unused-vars
@@ -17,6 +19,12 @@ type AppPropsWithLayout = AppProps & {
 export default function App({Component, pageProps}: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout ?? ((page) => <MainLayout>{page}</MainLayout>);
+  useEffect(() => {
+    getRequest({
+      url: URLs.user,
+      showToast: true,
+    });
+  }, []);
   return (
     <StyleProvider hashPriority="high">
       {getLayout(<Component {...pageProps} />)}
